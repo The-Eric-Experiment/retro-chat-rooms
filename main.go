31434c9383df05b1eb1e7a03fede81e83374abf1
Eric Mackrodt <ericmackrodt@gmail.com>
2022-04-05 06:31:10 -0700

    custom template renderer to improve retro browser compatibility

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
@@ -37,8 +38,8 @@ var config = LoadConfig()
 
 var rooms = config.Rooms
 
-func getAllTemplates() []string {
-	templates := make([]string, 0)
+func getAllTemplates() map[string]string {
+	templates := make(map[string]string)
 	err := filepath.Walk(".",
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -48,7 +49,12 @@ func getAllTemplates() []string {
 			ext := filepath.Ext(path)
 
 			if ext == ".html" {
-				templates = append(templates, path)
+				name := filepath.Base(path)
+				b, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
+				templates[name] = string(b)
 			}
 
 			return nil
@@ -58,6 +64,57 @@ func getAllTemplates() []string {
 	}
 
 	return templates
+}
+
+func LoadTemplates(router *gin.Engine) {
+	funcMap := template.FuncMap{
+		"isUserNotNil":     isUserNotNil,
+		"formatMessage":    formatMessage,
+		"formatTime":       formatTime,
+		"formatNickname":   formatNickname,
+		"isMessageVisible": isMessageVisible,
+		"isMessageToSelf":  isMessageToSelf,
+		"countUsers":       countUsers,
+	}
+
+	templates := getAllTemplates()
+
+	t := template.New("").Delims("{{", "}}").Funcs(funcMap)
+
+	for filename, content := range templates {
+		defineExpr, err := regexp.Compile("\\{\\{[\\s]*define[\\s]+\"[^\"]+\"[\\s]*}}")
+		if err != nil {
+			panic(err)
+		}
+		tagStartExpr, err := regexp.Compile("(>(?:[\\s\n\r])+)")
+		if err != nil {
+			panic(err)
+		}
+
+		tagEndExpr, err := regexp.Compile("((?:[\\s\n\r])+<)")
+		if err != nil {
+			panic(err)
+		}
+		spaceExpr, err := regexp.Compile("[\\s\r\n]+")
+		if err != nil {
+			panic(err)
+		}
+		contained := "{{ define \"" + filename + "\" }}" + content + "{{end}}"
+		if defineExpr.MatchString(content) {
+			contained = content
+		}
+
+		final := spaceExpr.ReplaceAllString(
+			tagEndExpr.ReplaceAllString(
+				tagStartExpr.ReplaceAllString(contained, ">"), "<",
+			),
+			" ",
+		)
+
+		t = template.Must(t.Parse(final))
+	}
+
+	router.SetHTMLTemplate(t)
 }
 
 func main() {
@@ -72,19 +129,9 @@ func main() {
 
 	router.Use(static.Serve("/public", static.LocalFile("./public", true)))
 
-	router.SetFuncMap(template.FuncMap{
-		"isUserNotNil":     isUserNotNil,
-		"formatMessage":    formatMessage,
-		"formatTime":       formatTime,
-		"formatNickname":   formatNickname,
-		"isMessageVisible": isMessageVisible,
-		"isMessageToSelf":  isMessageToSelf,
-		"countUsers":       countUsers,
-	})
+	// router.SetFuncMap()
 
-	templates := getAllTemplates()
-
-	router.LoadHTMLFiles(templates...)
+	LoadTemplates(router)
 
 	router.GET("/", GetIndex)
 	router.GET("/room/:id", GetRoom)
