9f6ad2cba6aaaa6805cd958df9e94866049b7145
Eric Mackrodt <ericmackrodt@gmail.com>
2022-04-09 07:01:48 -0700

    use configured files

--- public/hs.js
+++
@@ -1,5 +0,0 @@
-
-    
-    top.window.listener.document.write('<html><head><meta http-equiv="PRAGMA" content="NO-CACHE" /><title>Chat window</title></head><body bgcolor="#EEEEEE"> [01:16:35 PM] \u003cstrong\u003e\u003cfont color=\u0022#FF7F00\u0022\u003edon\u0027t cry a hgave\u003c\/font\u003e\u003c\/strong\u003e has joined the room! <br /><br /></body></html>')
-    
-      
